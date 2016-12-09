f05084e4957f77fabc4a55d070c8779c14a23ac8
Willwin Wang <willwin.wang@rea-group.com>
2016-12-10 00:36:44 +1100

    Automatically commited

---
+++ backups.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+LOCATION="restore.sh"
+ 
+#Change to script directory
+cd `dirname "$0"`
+ 
+#Dump Header
+echo '#/bin/bash' > "restore.sh"
+ 
+#Dump homebrew installion
+echo 'ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"' >> "restore.sh"
+ 
+#Dump update brew
+echo 'brew update' >> "restore.sh"
+echo 'brew upgrade' >> "restore.sh"
+ 
+#Dump taps
+brew tap | while read tap; 
+do
+  echo "brew tap $tap" >> "restore.sh"
+done
+
+#Dump brew
+brew list | while read item;
+do
+  echo "brew install $item" >> "restore.sh"
+done
+
+#Dump cask
+brew cask list | while read item;
+do
+  echo "brew cask install $item" >> "restore.sh"
+done
+
+#Dump app store
+find /Applications -path '*Contents/_MASReceipt/receipt' -maxdepth 4 -print |\sed 's#.app/Contents/_MASReceipt/receipt#.app#g; s#/Applications/##' > appstore.txt
+
+#Dump applications
+ls /Applications > applications.txt
+
+#Cleanup
+echo 'brew cleanup' >> $LOCATION
+echo 'brew cask cleanup' >> $LOCATION
+
+git add -A
+git commit -m "Automatically commited"
+git push
+
---
+++ restore.sh
@@ -0,0 +1,52 @@
+#/bin/bash
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew update
+brew upgrade
+brew tap ==> Auto-updated Homebrew!
+brew tap Updated 1 tap (homebrew/core).
+brew tap ==> Updated Formulae
+brew tap eigen
+brew tap 
+brew tap caskroom/cask
+brew tap homebrew/core
+brew tap neovim/neovim
+brew install autoconf
+brew install autojump
+brew install automake
+brew install cask
+brew install cmake
+brew install direnv
+brew install emacs
+brew install gdbm
+brew install gettext
+brew install gnupg
+brew install hub
+brew install libgpg-error
+brew install libksba
+brew install libtermkey
+brew install libtool
+brew install libuv
+brew install libvterm
+brew install libyaml
+brew install msgpack
+brew install neovim
+brew install nvm
+brew install openssl
+brew install pcre
+brew install pkg-config
+brew install python
+brew install readline
+brew install sqlite
+brew install unibilium
+brew install zsh-completions
+brew install zsh-syntax-highlighting
+brew cask install alfred
+brew cask install dropbox
+brew cask install evernote
+brew cask install google-chrome
+brew cask install iterm2
+brew cask install keepassx
+brew cask install spectacle
+brew cask install visual-studio-code
+brew cleanup
+brew cask cleanup
