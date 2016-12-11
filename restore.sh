3cb5e8e5e05e8a4c7d9e0d2b0310df43be89355f
Willwin Wang <willwin.wang@rea-group.com>
2016-12-12 10:06:46 +1100

    Automatically commited

--- restore.sh
+++ restore.sh
@@ -3,9 +3,63 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew update
 brew upgrade
 brew tap ==> Auto-updated Homebrew!
-brew tap Updated 1 tap (homebrew/core).
+brew tap Updated 2 taps (homebrew/core, neovim/neovim).
+brew tap ==> New Formulae
+brew tap curaengine
+brew tap excel-compare
+brew tap jid
 brew tap ==> Updated Formulae
-brew tap eigen
+brew tap alot
+brew tap ammonite-repl
+brew tap antigen
+brew tap arangodb
+brew tap aws-sdk-cpp
+brew tap awscli
+brew tap cairo
+brew tap cairomm
+brew tap direnv
+brew tap dockviz
+brew tap docutils
+brew tap fantom
+brew tap filebeat
+brew tap flake8
+brew tap gnutls
+brew tap gsl
+brew tap gtk+3
+brew tap hadolint
+brew tap html-xml-utils
+brew tap idris
+brew tap ioping
+brew tap ios-sim
+brew tap ircd-hybrid
+brew tap iso-codes
+brew tap jasper
+brew tap kibana
+brew tap knot
+brew tap kubernetes-cli
+brew tap libgcrypt
+brew tap libsass
+brew tap logstash
+brew tap macvim
+brew tap minio-mc
+brew tap mpd
+brew tap oniguruma
+brew tap owfs
+brew tap pandoc
+brew tap pkcs11-helper
+brew tap sassc
+brew tap solr
+brew tap swiftformat
+brew tap termius
+brew tap terragrunt
+brew tap tomcat
+brew tap tor
+brew tap typescript
+brew tap vim
+brew tap wakatime-cli
+brew tap wavpack
+brew tap ==> Deleted Formulae
+brew tap unfs3
 brew tap 
 brew tap caskroom/cask
 brew tap homebrew/core
@@ -46,6 +100,7 @@ brew cask install evernote
 brew cask install google-chrome
 brew cask install iterm2
 brew cask install keepassx
+brew cask install slack
 brew cask install spectacle
 brew cask install visual-studio-code
 brew cleanup
