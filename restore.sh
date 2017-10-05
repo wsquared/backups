3b84f2e3ee0d2bd037377aae29f2891a8cb7b2ed
Willwin Wang <willwin.wang@rea-group.com>
2017-10-05 15:47:56 +1100

    Automatically commited

--- restore.sh
+++ restore.sh
@@ -3,140 +3,8 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew update
 brew upgrade
 brew tap ==> Auto-updated Homebrew!
-brew tap Updated 4 taps (caskroom/cask, caskroom/versions, homebrew/core, homebrew/php).
-brew tap ==> New Formulae
-brew tap cppad
-brew tap frugal
-brew tap geth
-brew tap gmsh
-brew tap gomplate
-brew tap gromacs
-brew tap mimic
-brew tap ncview
-brew tap nlopt
-brew tap packmol
-brew tap stubby
-brew tap telnet
-brew tap ==> Updated Formulae
-brew tap akamai
-brew tap ammonite-repl
-brew tap angular-cli
-brew tap ansible
-brew tap ansible@2.0
-brew tap arangodb
-brew tap awscli
-brew tap binaryen
-brew tap bitcoin
-brew tap boost
-brew tap boost-bcp
-brew tap boost-mpi
-brew tap boost-python
-brew tap btfs
-brew tap cclive
-brew tap ceylon
-brew tap cimg
-brew tap clasp
-brew tap cli53
-brew tap clib
-brew tap conjure-up
-brew tap cucumber-cpp
-brew tap datamash
-brew tap docker
-brew tap docker-completion
-brew tap druid
-brew tap dspdfviewer
-brew tap efl
-brew tap embulk
-brew tap etcd
-brew tap folly
-brew tap fstar
-brew tap get-flash-videos
-brew tap get_iplayer
-brew tap ghq
-brew tap gitlab-ci-multi-runner
-brew tap gmic
-brew tap gnutls
-brew tap go
-brew tap godep
-brew tap gofabric8
-brew tap gopass
-brew tap grakn
-brew tap gtk+3
-brew tap harfbuzz
-brew tap heroku
-brew tap hicolor-icon-theme
-brew tap homebrew/php/phan
-brew tap homebrew/php/phan@0.8
-brew tap homebrew/php/php-cs-fixer
-brew tap iamy
-brew tap imagemagick
-brew tap imagemagick@6
-brew tap influxdb
-brew tap innoextract
-brew tap instead
-brew tap jenkins
-brew tap jhipster
-brew tap lean-cli
-brew tap ledger
-brew tap libbitcoin
-brew tap libphonenumber
-brew tap libxml2
-brew tap lxc
-brew tap macosvpn
-brew tap mapcrafter
-brew tap metaproxy
-brew tap micropython
-brew tap mingw-w64
-brew tap mkvtoolnix
-brew tap mongo-cxx-driver
-brew tap mpd
-brew tap ncmpcpp
-brew tap osm2pgrouting
-brew tap osmium-tool
-brew tap osquery
-brew tap osrm-backend
-brew tap paket
-brew tap parallel
-brew tap passenger
-brew tap pcb2gcode
-brew tap pdftoedn
-brew tap percona-server@5.5
-brew tap points2grid
-brew tap rebar@3
-brew tap ripgrep
-brew tap selenium-server-standalone
-brew tap shairport-sync
-brew tap snapcraft
-brew tap source-highlight
-brew tap sourcekitten
-brew tap stella
-brew tap swiftformat
-brew tap swiftgen
-brew tap swimat
-brew tap teensy_loader_cli
-brew tap thefuck
-brew tap timedog
-brew tap tippecanoe
-brew tap transcrypt
-brew tap uhd
-brew tap unbound
-brew tap vagrant-completion
-brew tap vdirsyncer
-brew tap vim
-brew tap vowpal-wabbit
-brew tap wesnoth
-brew tap whois
-brew tap widelands
-brew tap winetricks
-brew tap yadm
-brew tap youtube-dl
-brew tap zabbix
-brew tap zstd
-brew tap ==> Deleted Formulae
-brew tap go-gui
-brew tap kafka@0.80
-brew tap phantomjs@1.92
-brew tap phantomjs@1.98
+brew tap Updated 1 tap (caskroom/cask).
+brew tap No changes to formulae.
 brew tap 
 brew tap caskroom/cask
 brew tap caskroom/versions
@@ -171,6 +39,7 @@ brew install hub
 brew install icu4c
 brew install ipcalc
 brew install jpeg
+brew install jq
 brew install libassuan
 brew install libffi
 brew install libgcrypt
@@ -194,6 +63,7 @@ brew install nettle
 brew install node
 brew install npth
 brew install nvm
+brew install oniguruma
 brew install openssl
 brew install p11-kit
 brew install pcre
@@ -208,6 +78,7 @@ brew install readline
 brew install ruby
 brew install sbt
 brew install scala
+brew install shush
 brew install sqlite
 brew install unibilium
 brew install unixodbc
@@ -220,6 +91,7 @@ brew cask install alfred
 brew cask install dropbox
 brew cask install evernote
 brew cask install firefox
+brew cask install gimp
 brew cask install google-chrome
 brew cask install iterm2
 brew cask install java
