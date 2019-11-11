74bd6380f4272fc071d1a0e39d3f35f582f4e9b9
Willwin Wang <willwin.wang@rea-group.com>
2019-11-12 08:08:51 +1100

    Automatically commited

--- backups.sh
+++ backups.sh
@@ -40,7 +40,7 @@ find /Applications -path '*Contents/_MASReceipt/receipt' -maxdepth 4 -print |\se
 ls /Applications > applications.txt
 
 #Dump zshrc
-cp -R ~/.zshrc ./zsh/.
+cp -R ~/.zshrc ./zshrc/.
 
 #Dump vim
 cp -R ~/.vim_runtime/vimrcs/. ./vimrcs
